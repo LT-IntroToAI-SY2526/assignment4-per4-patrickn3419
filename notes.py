9444c0d79f73510563bcb1e6bde4af813a84209d
Patrick Nyman <pnyman@cps.edu>
2025-10-24 11:07:12 -0500

    Added to Dog class

--- notes.py
+++ notes.py
@@ -12,13 +12,12 @@ class Dog:
         name - the name of the dog
         age - the age of the dog
     """
-    def __init__(self, breed, fur_color, name, age) -> None:
+    def __init__(self, breed = "dog", fur_color = "brown", name = "no_name", age = 1) -> None:
         """Initialize a new Dog with breed, fur_color, name, and age"""
         self.breed = breed
         self.fur_color = fur_color
         self.name = name
         self.age = age
-
 
     def __str__(self) -> str:
         """String representation of a dog"""
@@ -27,8 +26,27 @@ class Dog:
     def bark(self):
         print(f"{self.name} says: Woof, woof!")
 
+    def birthday(self):
+        """celebrate the dog's birthday"""
+        self.age += 1
+        print(f"{self.name} is celebrating their {self.age} birthday!")
+
+    def paint_dog(self, new_color):
+        """Changes the fur_color of the dog"""
+        old_color = self.fur_color
+        self.fur_color = new_color
+        print(f"{self.name} changed their fur color from {old_color} to {new_color}.")
+
+
 if __name__ == "__main__":
     patrick_dog = Dog("rottweiler", "black", "lunch", 10)
+    matthew_dog = Dog(breed = "labrador", name = "bella", age = 1)
+    default_dog = Dog()
 
+    print(default_dog)
     print(patrick_dog)
+    print(matthew_dog)
+
     patrick_dog.bark()
+    patrick_dog.birthday()
+    patrick_dog.paint_dog("purple")
